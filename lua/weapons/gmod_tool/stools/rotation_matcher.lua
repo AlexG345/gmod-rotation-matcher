82c8f888b3aaf37f56cb2a23fe0e1b94b0f63664
AlexG345 <alexgernez1@gmail.com>
2026-03-28 14:17:43 +0100

    fixed bad file

--- lua/weapons/gmod_tool/stools/rotation_matcher.lua
+++ lua/weapons/gmod_tool/stools/rotation_matcher.lua
@@ -1,20 +1,42 @@
 local mode = TOOL.Mode
 
+local advBsKeys = (
+	SERVER and {
+		"forcelimit", "torquelimit",
+		"xmin", "ymin", "zmin",
+		"xmax", "ymax", "zmax",
+		"xfric", "yfric", "zfric",
+		"onlyrotation", "nocollide",
+	} or nil
+)
 
-if SERVER then
+local advBsValues = (
+	SERVER and {
+		0, 0,
+		-0.01, -0.01, -0.01,
+		0.01, 0.01, 0.01,
+		0, 0, 0,
+		1, 0,
+	} or nil
+)
 
-	local flags = bit.bor( FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED )
+local advBsKeyValues = SERVER and {} or nil
+for i, value in ipairs( SERVER and advBsValues or {} ) do
+	advBsKeyValues[advBsKeys[i]] = value
+end
 
-	-- Create server console variables here.
+advBsKeys = nil
 
-else
 
-	TOOL.Category	= "Construction"
-	TOOL.Name		= "Template Tool"
+if CLIENT then
+
+	TOOL.Category	= "Constraints"
+	TOOL.Name		= "Rotation Matcher"
 
 	TOOL.Information = {
 		{ name = "left_0", stage = 0 },
 		{ name = "left_1", stage = 1 },
+		{ name = "reload" },
 	}
 
 	TOOL.ClientConVar = {
@@ -30,27 +52,67 @@ else
 
 	l( "listname", "Rotation Matcher" )
 	l( "name", TOOL.Name )
-	l( "desc", "Make the rotation of two props match each other." )
+	l( "desc", "Make the rotation of two entities match each other through the use of a constraint." )
 	l( "left_0", "Start by selecting the first entity to constrain.")
 	l( "left_1", "Finish by selecting the second entity to constrain.")
+	l( "reload", "Remove any constraint that has the same values as those created by this tool.")
 
 end
+
+
+
+
+-- Similar to constraint.FindConstraints but you can specify specific values.
+-- It also returns the found count and the original constraint table
+-- The keys are preserved between the original and the found constraint table
+local FindConstraints = ( SERVER and function( ent, constrType, values )
+
+	local constrs = ent.Constraints
+	if not constrs or ( next( constrs ) == nil ) then return {}, 0, {} end
+
+	local foundConstrs = {}
+	local foundCount = 0
+
+	for k, constr in pairs( constrs ) do
+
+		if IsValid( constr ) and constr.Type == constrType then
+
+			local hasRightValues = true
+			for key, value in pairs( values ) do
+				if constr[key] ~= value then
+					hasRightValues = false
+					break
+				end
+			end
+
+			if hasRightValues then
+				foundConstrs[k] = constr
+				foundCount = foundCount + 1
+			end
+
+		end
+
+	end
+
+	return foundConstrs, foundCount, constrs
+
+end) or nil
+
+
 
 
 function TOOL:LeftClick( trace )
 
 	local ent = trace.Entity
 
-
 	if IsValid( ent ) and ent:IsPlayer() then return false end
 
 	-- If there's no physics object then we can't constraint it!
-	if SERVER and !util.IsValidPhysicsObject( ent, trace.PhysicsBone ) then return false end
+	if SERVER and not util.IsValidPhysicsObject( ent, trace.PhysicsBone ) then return false end
 
 	local iNum = self:NumObjects()
-	local physBone = trace.PhysicsBone
-	local phys = ent:GetPhysicsObjectNum( physBone )
-	self:SetObject( iNum + 1, ent, trace.HitPos, phys, physBone, trace.HitNormal )
+	local phys = ent:GetPhysicsObjectNum( trace.PhysicsBone )
+	self:SetObject( iNum + 1, ent, trace.HitPos, phys, trace.PhysicsBone, trace.HitNormal )
 
 	if CLIENT then
 		if iNum > 0 then self:ClearObjects() end
@@ -65,20 +127,28 @@ function TOOL:LeftClick( trace )
 	if iNum == 1 then
 
 		local ply = self:GetOwner()
-		if !ply:CheckLimit( "constraints" ) then
+		if not ply:CheckLimit( "constraints" ) then
 			self:ClearObjects()
 			return false
 		end
 
+		local ent1, ent2 = self:GetEnt( 1 ), self:GetEnt( 2 )
+
+		local foundConstrs, foundCount = FindConstraints( ent1, "AdvBallsocket", advBsKeyValues )
+
+		for k, constr in pairs( foundConstrs ) do
+			if constr.Ent1 == ent2 or constr.Ent2 == ent2 then
+				ply:ChatPrint( "[ERROR] This entity already has " .. foundCount .. " such constraint" .. (foundCount > 1 and "s." or ".") )
+				self:ClearObjects()
+				return false
+			end
+		end
+
 		local constr = constraint.AdvBallsocket(
-			self:GetEnt( 1 ), self:GetEnt( 2 ),		-- entities
+			ent1, ent2,								-- entities
 			self:GetBone( 1 ), self:GetBone( 2 ),	-- bones
 			vector_origin, vector_origin,			-- local positions
-			0, 0,									-- force/torque limits
-			-0.01, -0.01, -0.01,					-- axis minimums
-			0.01, 0.01, 0.01,						-- axis maximums
-			0, 0, 0,								-- axis frictions
-			1, 0									-- only rotation, no collide (0 for false, 1 for true)
+			unpack(advBsValues)
 		)
 
 		if IsValid( constr ) then
@@ -102,6 +172,40 @@ function TOOL:LeftClick( trace )
 	return true
 
 end
+
+
+
+
+function TOOL:Reload( trace )
+
+	if CLIENT then return true end
+
+	local ent = trace.Entity
+	if not IsValid( ent ) or ent:IsPlayer() then return false end
+
+	local constrsToRemove, removeCount, constrs = FindConstraints( ent, "AdvBallsocket", advBsKeyValues )
+
+	for k, constr in pairs( constrsToRemove ) do
+		constr:Remove()
+		constrs[k] = nil
+	end
+
+	if removeCount > 0 then
+		self:GetOwner():ChatPrint("Removed " .. removeCount .. " constraint" .. (removeCount > 1 and "s." or ".") )
+		return true
+	end
+	return false
+
+end
+
+
+
+
+function TOOL:Holster()
+	self:ClearObjects()
+end
+
+
 
 
 function TOOL.BuildCPanel( cPanel )
